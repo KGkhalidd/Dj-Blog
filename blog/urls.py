c2d0a879bc0fce64091b9e3ff2002817037b241e
khaled <khaledgamal1345@gmail.com>
2023-03-12 12:41:49 +0200

    added templates & statics & views & urls and 404 page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.starting_page, name='starting-page'),
     path('posts/', views.posts, name='posts-page'),
+    
     # the first slug just to trigger it is a name with some dashes -- and the second we will pass it in views
     path('posts/<slug:slug>/', views.post_detail, name='post-detail-page'),
 
